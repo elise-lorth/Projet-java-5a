43b9564acc7125ff93e04849a92d293016449d99
unknown <elise.lorthiois@epfedu.fr>
2021-10-04 11:35:46 +0200

    Fusion avec les modification des edit/create

--- src/main/java/io/takima/demo/controller/LibraryController.java
+++ src/main/java/io/takima/demo/controller/LibraryController.java
@@ -15,7 +15,6 @@ import java.util.Optional;
  *
  */
 @Controller
-@RequestMapping(name = "test")
 public class LibraryController {
 
     private final UserDAO userDAO;
@@ -29,6 +28,12 @@ public class LibraryController {
         m.addAttribute("users", userDAO.findAll());
         return "accueilPublic";
     }
+    @GetMapping("/accueilPublic")
+    public String PublicPage(Model m) {
+        m.addAttribute("users", userDAO.findAll());
+        return "accueilPublic";
+    }
+
     @GetMapping("/accueilAdmin")
     public String AdminPage(Model m) {
         m.addAttribute("users", userDAO.findAll());
@@ -57,8 +62,18 @@ public class LibraryController {
     public RedirectView createNewUser(@ModelAttribute User user, RedirectAttributes attrs) {
         attrs.addFlashAttribute("message", "Utilisateur ajouté avec succès");
         userDAO.save(user);
-        return new RedirectView("/");
+        return new RedirectView("/accueilAdmin");
     }
+
+
+    @GetMapping("/deleteMember/{id}")
+    public RedirectView deleteUser(@PathVariable("id") long id, Model model) {
+        User user = userDAO.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userDAO.delete(user);
+        return new RedirectView("/accueilAdmin");
+    }
+
 
     @GetMapping("/edit/{id}")
     public String editUserPage(@PathVariable("id") long id, Model m) {
