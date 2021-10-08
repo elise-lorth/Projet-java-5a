632cec45657a044ec0669c06cdada4e202dabc08
elise-lorth <elise.lorthiois@epfedu.fr>
2021-10-08 11:13:29 +0200

    Amélioration du contenu par défaut de la bdd

--- initdb/2_DEFAULT_ENTRIES.sql
+++ initdb/2_DEFAULT_ENTRIES.sql
@@ -13,17 +13,17 @@ INSERT INTO defaultdb.users (first_name, last_name, age, email) VALUES ('Elise',
 
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Promenons-nous dans les bois',7,'3','2','1','images/forest.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Wonderwall',8,'4','2','1','images/great-wall-of-china.png');
-INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('L étrange maison de M. Jack',4,'2','2','1','images/haunted-house.png');
+INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('L\'étrange maison de M. Jack',4,'2','2','1','images/haunted-house.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('This is sparta',4,'2','1','1','images/coliseum.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('La vie en rose',6,'3','2','1','images/eiffel-tower.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Sexy beach',8,'4','2','1','images/beach.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Camping paradis',3,'2','2','1','images/camping.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Gitaanooo ',2,'1','2','1','images/caravan.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('La route de la soie',4,'2','2','1','images/desert.png');
-INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('L amour est dans le pré',5,'2','2','1','images/farm.png');
-INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Un jour j irais à New-York avec toi' ,9,'5','2','1','images/new-york.png');
+INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('L\'amour est dans le pré',5,'2','2','1','images/farm.png');
+INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Un jour j\'irais à New-York avec toi' ,9,'5','2','1','images/new-york.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Le château arc-en-fiel',10,'5','2','1','images/palace.png');
-INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Sous l océaaan',7,'3','2','1','images/sea-bottom.png');
+INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Sous l\'océaaan',7,'3','2','1','images/sea-bottom.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Twilight',8,'4','2','1','images/sky.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Blanc comme neige',5,'2','2','1','images/snowman.png');
 INSERT INTO defaultdb.rooms (name, capacity, screen, tablet, board, icon) VALUE ('Volcanoroom',6,'3','2','1','images/volcano.png');
